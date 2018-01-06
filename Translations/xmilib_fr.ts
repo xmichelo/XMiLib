ee0d3fc231493b4e27cdecd578aecf2323a4fa04
Xavier Michelon <xmichelo@gmail.com>
2018-01-06 13:48:34 +0100

    Introduced French translation.

---
+++ Translations/xmilib_fr.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>DebugLogWindow</name>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="51"/>
+        <source>Show:</source>
+        <translation>Afficher:</translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="58"/>
+        <source>Infos</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="68"/>
+        <source>Warnings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="78"/>
+        <source>Errors</source>
+        <translation>Erreurs</translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="101"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="118"/>
+        <source>C&amp;lear Log</source>
+        <translation>E&amp;ffacer le Log</translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="108"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="126"/>
+        <source>&amp;Close</source>
+        <oldsource>Close</oldsource>
+        <translation>&amp;Fermer</translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="121"/>
+        <source>Ctrl+X</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>StyleSheetEditor</name>
+    <message>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
+        <source>&amp;OK</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+    <message>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
+        <source>&amp;Apply</source>
+        <translation>A&amp;ppliquer</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadedOperationDialog</name>
+    <message>
+        <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
+        <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::DebugLog</name>
+    <message>
+        <location filename="../DebugLog/DebugLog.cpp" line="229"/>
+        <source>Date/Time</source>
+        <translation>Date/Heure</translation>
+    </message>
+    <message>
+        <location filename="../DebugLog/DebugLog.cpp" line="230"/>
+        <source>Message</source>
+        <translation>Message</translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::ThreadedOperationDialog</name>
+    <message>
+        <location filename="../ThreadedOperation/ThreadedOperationDialog.cpp" line="125"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+</context>
+</TS>
