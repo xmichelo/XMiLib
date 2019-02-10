b3861c584b4f255c193e65717ef12855c914da88
Xavier Michelon <xmichelo@users.noreply.github.com>
2019-02-10 06:26:23 +0100

    Added file for constants.

---
+++ XMiLib/XMiLibConstants.cpp
@@ -0,0 +1,26 @@
+﻿/// \file
+/// \author 
+///
+/// \brief Implementation of constants for the XMiLib library
+///  
+/// Copyright (c) . All rights reserved.  
+/// Licensed under the MIT License. See LICENSE file in the project root for full license information. 
+
+
+#include "stdafx.h"
+#include "XMiLibConstants.h"
+
+
+namespace xmilib {
+
+
+namespace constants {
+
+
+Qt::WindowFlags const kDefaultDialogFlags = Qt::WindowCloseButtonHint | Qt::WindowTitleHint;
+
+
+} // namespace constants
+
+
+} // namespace xmilib
---
+++ XMiLib/XMiLibConstants.h
@@ -0,0 +1,29 @@
+﻿/// \file
+/// \author 
+///
+/// \brief Declaration of constants for the XMiLib library
+///  
+/// Copyright (c) . All rights reserved.  
+/// Licensed under the MIT License. See LICENSE file in the project root for full license information. 
+
+
+#ifndef XMILIB_CONSTANTS_H
+#define XMILIB_CONSTANTS_H
+
+
+namespace xmilib {
+
+
+namespace constants {
+
+
+extern Qt::WindowFlags const kDefaultDialogFlags; ///< The default window flags for dialogs
+
+
+} // namespace constants
+
+
+} // namespace xmilib
+
+
+#endif // #ifndef XMILIB_CONSTANTS_H
