62eebcb54512f3c44d3bb64becd98e92834297fe
Xavier Michelon <xmichelo@users.noreply.github.com>
2019-03-08 07:29:59 +0100

    Introduced SingleInstanceApp.

---
+++ XMiLib/SingleInstanceApp.cpp
@@ -0,0 +1,37 @@
+﻿/// \file
+/// \author Xavier Michelon
+///
+/// \brief Implementation of Qt application class that allows only a single instance
+///  
+/// Copyright (c) . All rights reserved.  
+/// Licensed under the MIT License. See LICENSE file in the project root for full license information. 
+
+
+#include "stdafx.h"
+#include "SingleInstanceApp.h"
+
+
+namespace xmilib {
+
+
+//**********************************************************************************************************************
+/// \param[in] appKey The key identifying the application
+//**********************************************************************************************************************
+SingleInstanceApplication::SingleInstanceApplication(QString const& appKey)
+   : sharedMem_(appKey)
+{
+   isFirstInstance_ = sharedMem_.create(sizeof(WId));
+}
+
+
+//**********************************************************************************************************************
+/// \return true if no other instance of the application was running when this instance was created
+//**********************************************************************************************************************
+bool SingleInstanceApplication::isFirstInstance() const
+{
+   return isFirstInstance_;
+}
+
+
+}
+
---
+++ XMiLib/SingleInstanceApp.h
@@ -0,0 +1,42 @@
+﻿/// \file
+/// \author Xavier Michelon
+///
+/// \brief Declaration of class that allows only a single instance of an application.
+///  
+/// Copyright (c) . All rights reserved.  
+/// Licensed under the MIT License. See LICENSE file in the project root for full license information. 
+
+
+#ifndef XMILIB_SINGLE_INSTANCE_APP_H
+#define XMILIB_SINGLE_INSTANCE_APP_H
+
+
+namespace xmilib {
+
+
+//**********************************************************************************************************************
+/// \brief A class managing a single instance of an application
+//**********************************************************************************************************************
+class SingleInstanceApplication
+{
+public: // member functions
+   SingleInstanceApplication(QString const& appKey); ///< Default constructor
+   SingleInstanceApplication(SingleInstanceApplication const&) = delete; ///< Disabled copy-constructor
+   SingleInstanceApplication(SingleInstanceApplication&&) = delete; ///< Disabled assignment copy-constructor
+   ~SingleInstanceApplication() = default; ///< Destructor
+   SingleInstanceApplication& operator=(SingleInstanceApplication const&) = delete; ///< Disabled assignment operator
+   SingleInstanceApplication& operator=(SingleInstanceApplication&&) = delete; ///< Disabled move assignment operator
+   bool isFirstInstance() const; ///< Check whether this instance is the first instance of the application
+
+private:
+   QSharedMemory sharedMem_; ///< The shared memory segment
+   bool isFirstInstance_ { false }; ///< Is this instance the first instance of the application
+};
+
+
+
+
+} // namespace xmilib
+
+
+#endif // #ifndef XMILIB_SINGLE_INSTANCE_APP_H
