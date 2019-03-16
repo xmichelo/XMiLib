b513d3973fcf54ad8e3fbfd2314176f278617e3e
Xavier Michelon <xmichelo@users.noreply.github.com>
2019-03-16 15:37:58 +0100

    SingleInstanceApp is now fully functional.

--- XMiLib/SingleInstanceApp.h
+++ XMiLib/SingleInstanceApp.h
@@ -17,20 +17,34 @@ namespace xmilib {
 //**********************************************************************************************************************
 /// \brief A class managing a single instance of an application
 //**********************************************************************************************************************
-class SingleInstanceApplication
+class SingleInstanceApplication: public QObject
 {
+   Q_OBJECT;
 public: // member functions
-   SingleInstanceApplication(QString const& appKey); ///< Default constructor
-   SingleInstanceApplication(SingleInstanceApplication const&) = delete; ///< Disabled copy-constructor
-   SingleInstanceApplication(SingleInstanceApplication&&) = delete; ///< Disabled assignment copy-constructor
-   ~SingleInstanceApplication() = default; ///< Destructor
-   SingleInstanceApplication& operator=(SingleInstanceApplication const&) = delete; ///< Disabled assignment operator
-   SingleInstanceApplication& operator=(SingleInstanceApplication&&) = delete; ///< Disabled move assignment operator
-   bool isFirstInstance() const; ///< Check whether this instance is the first instance of the application
+   SingleInstanceApplication(QString const& appKey); ///< Default constructor.
+   SingleInstanceApplication(SingleInstanceApplication const&) = delete; ///< Disabled copy-constructor.
+   SingleInstanceApplication(SingleInstanceApplication&&) = delete; ///< Disabled assignment copy-constructor.
+   ~SingleInstanceApplication() = default; ///< Destructor.
+   SingleInstanceApplication& operator=(SingleInstanceApplication const&) = delete; ///< Disabled assignment operator.
+   SingleInstanceApplication& operator=(SingleInstanceApplication&&) = delete; ///< Disabled move assignment operator.
+   bool isFirstInstance() const; ///< Check whether this instance is the first instance of the application.
+   bool isValid() const; ///< Is the instance of the class valid
 
-private:
-   QSharedMemory sharedMem_; ///< The shared memory segment
-   bool isFirstInstance_ { false }; ///< Is this instance the first instance of the application
+signals:
+   void anotherInstanceWasLaunched(); ///< Signal emitted when another instance is launched.
+
+private slots:
+   void onTimer(); ///< Slot for the timer.
+
+private: // member functions
+   qint32 sharedValue(); /// return the value placed in the shared memory segment
+   void setSharedValue(qint32 value); /// return the value placed in the shared memory segment
+
+private: // data membems
+   QSharedMemory sharedMem_; ///< The shared memory segment.
+   QTimer timer_; ///< The timer for check the shared memory segment.
+   bool isFirstInstance_ { false }; ///< Is this instance the first instance of the application.
+   bool isValid_ { true };
 };
 
 
