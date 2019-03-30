25a0ecc007626facef3af411e0b48c53b151d534
Xavier Michelon <xmichelo@users.noreply.github.com>
2019-03-30 16:00:50 +0100

    Added Russian translation, courtesy of Valerij Romanovskij.

---
+++ XMiLib/Translations/xmilib_ru.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>StringListEditorDialog</name>
+    <message>
+        <source>&amp;OK</source>
+        <translation>&amp;ОК</translation>
+    </message>
+    <message>
+        <source>&amp;Add</source>
+        <translation>&amp;Добавить</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Отмена</translation>
+    </message>
+    <message>
+        <source>&amp;Remove</source>
+        <translation>&amp;Удалить</translation>
+    </message>
+</context>
+<context>
+    <name>StyleSheetEditor</name>
+    <message>
+        <source>&amp;OK</source>
+        <translation>&amp;ОК</translation>
+    </message>
+    <message>
+        <source>&amp;Apply</source>
+        <translation>&amp;Применить</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::ThreadedOperationDialog</name>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::StringListEditorDialog</name>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <source>Internal error: %1</source>
+        <translation>Внутренняя ошибка: %1</translation>
+    </message>
+</context>
+<context>
+    <name>DebugLogWindow</name>
+    <message>
+        <source>Infos</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <source>Show:</source>
+        <translation>Показать:</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Закрыть</translation>
+    </message>
+    <message>
+        <source>Ctrl+X</source>
+        <translation>Ctrl+X</translation>
+    </message>
+    <message>
+        <source>Errors</source>
+        <translation>Ошибки</translation>
+    </message>
+    <message>
+        <source>Warnings</source>
+        <translation>Предупреждения</translation>
+    </message>
+    <message>
+        <source>C&amp;lear Log</source>
+        <translation>&amp;Очистить журнал</translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::DebugLog</name>
+    <message>
+        <source>Message</source>
+        <translation>Сообщение</translation>
+    </message>
+    <message>
+        <source>Date/Time</source>
+        <translation>Дата/Время</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadedOperationDialog</name>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Отмена</translation>
+    </message>
+</context>
+</TS>
