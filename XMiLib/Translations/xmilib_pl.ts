0994a1cf52110ba03a0d58b923ae058246d71e97
Xavier Michelon <xmichelo@users.noreply.github.com>
2020-03-31 07:00:50 +0200

    Added Polish translation files.

---
+++ XMiLib/Translations/xmilib_pl.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>DebugLogWindow</name>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="51"/>
+        <source>Show:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="58"/>
+        <source>Infos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="68"/>
+        <source>Warnings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="78"/>
+        <source>Errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="101"/>
+        <location filename="DebugLog/DebugLogWindow.ui" line="118"/>
+        <source>C&amp;lear Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="108"/>
+        <location filename="DebugLog/DebugLogWindow.ui" line="126"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="121"/>
+        <source>Ctrl+X</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StringListEditorDialog</name>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="58"/>
+        <source>&amp;Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="65"/>
+        <source>&amp;Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="106"/>
+        <source>&amp;OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="113"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StyleSheetEditor</name>
+    <message>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
+        <source>&amp;OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
+        <source>&amp;Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThreadedOperationDialog</name>
+    <message>
+        <location filename="ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
+        <location filename="ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::DebugLog</name>
+    <message>
+        <location filename="DebugLog/DebugLog.cpp" line="225"/>
+        <source>Date/Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLog.cpp" line="226"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::StringListEditorDialog</name>
+    <message>
+        <location filename="String/StringListEditorDialog.cpp" line="104"/>
+        <location filename="String/StringListEditorDialog.cpp" line="128"/>
+        <location filename="String/StringListEditorDialog.cpp" line="158"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.cpp" line="104"/>
+        <location filename="String/StringListEditorDialog.cpp" line="128"/>
+        <location filename="String/StringListEditorDialog.cpp" line="158"/>
+        <source>Internal error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::ThreadedOperationDialog</name>
+    <message>
+        <location filename="ThreadedOperation/ThreadedOperationDialog.cpp" line="134"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
