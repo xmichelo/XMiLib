30cdcf8c0a3ae521574ec019a939db010b2dc9e9
Xavier Michelon <xmichelo@users.noreply.github.com>
2020-04-02 07:09:19 +0200

    Added Polish translation.

--- XMiLib/Translations/xmilib_pl.ts
+++ XMiLib/Translations/xmilib_pl.ts
@@ -4,132 +4,132 @@
 <context>
     <name>DebugLogWindow</name>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="51"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="51"/>
         <source>Show:</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyświetl:</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="58"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="58"/>
         <source>Infos</source>
-        <translation type="unfinished"></translation>
+        <translation>Informacje</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="68"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="68"/>
         <source>Warnings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ostrzeżenia</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="78"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="78"/>
         <source>Errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Błędy</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="101"/>
-        <location filename="DebugLog/DebugLogWindow.ui" line="118"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="101"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="118"/>
         <source>C&amp;lear Log</source>
-        <translation type="unfinished"></translation>
+        <translation>W&amp;yczyść Dziennik Zdarzeń</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="108"/>
-        <location filename="DebugLog/DebugLogWindow.ui" line="126"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="108"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="126"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zamknij</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLogWindow.ui" line="121"/>
+        <location filename="../DebugLog/DebugLogWindow.ui" line="121"/>
         <source>Ctrl+X</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+X</translation>
     </message>
 </context>
 <context>
     <name>StringListEditorDialog</name>
     <message>
-        <location filename="String/StringListEditorDialog.ui" line="58"/>
+        <location filename="../String/StringListEditorDialog.ui" line="58"/>
         <source>&amp;Add</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Dodaj</translation>
     </message>
     <message>
-        <location filename="String/StringListEditorDialog.ui" line="65"/>
+        <location filename="../String/StringListEditorDialog.ui" line="65"/>
         <source>&amp;Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Usuń</translation>
     </message>
     <message>
-        <location filename="String/StringListEditorDialog.ui" line="106"/>
+        <location filename="../String/StringListEditorDialog.ui" line="106"/>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;OK</translation>
     </message>
     <message>
-        <location filename="String/StringListEditorDialog.ui" line="113"/>
+        <location filename="../String/StringListEditorDialog.ui" line="113"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Anuluj</translation>
     </message>
 </context>
 <context>
     <name>StyleSheetEditor</name>
     <message>
-        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
-        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;OK</translation>
     </message>
     <message>
-        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
-        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Anuluj</translation>
     </message>
     <message>
-        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
-        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
+        <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
         <source>&amp;Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zastosuj</translation>
     </message>
 </context>
 <context>
     <name>ThreadedOperationDialog</name>
     <message>
-        <location filename="ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
-        <location filename="ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
+        <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
+        <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Anuluj</translation>
     </message>
 </context>
 <context>
     <name>xmilib::DebugLog</name>
     <message>
-        <location filename="DebugLog/DebugLog.cpp" line="225"/>
+        <location filename="../DebugLog/DebugLog.cpp" line="225"/>
         <source>Date/Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Data/Czas</translation>
     </message>
     <message>
-        <location filename="DebugLog/DebugLog.cpp" line="226"/>
+        <location filename="../DebugLog/DebugLog.cpp" line="226"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Wiadomość</translation>
     </message>
 </context>
 <context>
     <name>xmilib::StringListEditorDialog</name>
     <message>
-        <location filename="String/StringListEditorDialog.cpp" line="104"/>
-        <location filename="String/StringListEditorDialog.cpp" line="128"/>
-        <location filename="String/StringListEditorDialog.cpp" line="158"/>
+        <location filename="../String/StringListEditorDialog.cpp" line="104"/>
+        <location filename="../String/StringListEditorDialog.cpp" line="128"/>
+        <location filename="../String/StringListEditorDialog.cpp" line="158"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd</translation>
     </message>
     <message>
-        <location filename="String/StringListEditorDialog.cpp" line="104"/>
-        <location filename="String/StringListEditorDialog.cpp" line="128"/>
-        <location filename="String/StringListEditorDialog.cpp" line="158"/>
+        <location filename="../String/StringListEditorDialog.cpp" line="104"/>
+        <location filename="../String/StringListEditorDialog.cpp" line="128"/>
+        <location filename="../String/StringListEditorDialog.cpp" line="158"/>
         <source>Internal error: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd wewnętrzny: %1</translation>
     </message>
 </context>
 <context>
     <name>xmilib::ThreadedOperationDialog</name>
     <message>
-        <location filename="ThreadedOperation/ThreadedOperationDialog.cpp" line="134"/>
+        <location filename="../ThreadedOperation/ThreadedOperationDialog.cpp" line="134"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd</translation>
     </message>
 </context>
 </TS>
