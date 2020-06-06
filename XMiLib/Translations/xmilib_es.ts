c947f018d381812b7dd2367df47b7aa424bab22d
Xavier Michelon <xmichelo@users.noreply.github.com>
2020-06-06 11:07:45 +0200

    Added Spanish translation files.

--- XMiLib/Translations/xmilib_es.ts
+++ XMiLib/Translations/xmilib_es.ts
@@ -1,44 +1,44 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt_BR">
+<TS version="2.1" language="es_ES">
 <context>
     <name>DebugLogWindow</name>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="51"/>
         <source>Show:</source>
-        <translation>Mostrar:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="58"/>
         <source>Infos</source>
-        <translation>Informações</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="68"/>
         <source>Warnings</source>
-        <translation>Avisos</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="78"/>
         <source>Errors</source>
-        <translation>Erros</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="101"/>
         <location filename="../DebugLog/DebugLogWindow.ui" line="118"/>
         <source>C&amp;lear Log</source>
-        <translation>&amp;Limpar Registro</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="108"/>
         <location filename="../DebugLog/DebugLogWindow.ui" line="126"/>
         <source>&amp;Close</source>
-        <translation>Fe&amp;char</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="121"/>
         <source>Ctrl+X</source>
-        <translation>Ctrl+X</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -46,22 +46,22 @@
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="58"/>
         <source>&amp;Add</source>
-        <translation>&amp;Adicionar</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="65"/>
         <source>&amp;Remove</source>
-        <translation>&amp;Remover</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="106"/>
         <source>&amp;OK</source>
-        <translation>&amp;Ok</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="113"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Cancelar</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -70,19 +70,19 @@
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
         <source>&amp;OK</source>
-        <translation>&amp;Ok</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Cancelar</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
         <source>&amp;Apply</source>
-        <translation>&amp;Aplicar</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -91,7 +91,7 @@
         <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
         <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Cancelar</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -99,12 +99,12 @@
     <message>
         <location filename="../DebugLog/DebugLog.cpp" line="225"/>
         <source>Date/Time</source>
-        <translation>Data/Hora</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLog.cpp" line="226"/>
         <source>Message</source>
-        <translation>Mensagem</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -114,14 +114,14 @@
         <location filename="../String/StringListEditorDialog.cpp" line="128"/>
         <location filename="../String/StringListEditorDialog.cpp" line="158"/>
         <source>Error</source>
-        <translation>Erro</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.cpp" line="104"/>
         <location filename="../String/StringListEditorDialog.cpp" line="128"/>
         <location filename="../String/StringListEditorDialog.cpp" line="158"/>
         <source>Internal error: %1</source>
-        <translation>Erro interno: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -129,7 +129,7 @@
     <message>
         <location filename="../ThreadedOperation/ThreadedOperationDialog.cpp" line="134"/>
         <source>Error</source>
-        <translation>Erro</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
