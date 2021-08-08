365b639a8e348dcf134186f5ee82293ce1297aec
Xavier Michelon <xavier@michelon.ch>
2021-08-08 08:54:08 +0200

    Updated translation files.

--- XMiLib/Translations/xmilib_pt.ts
+++ XMiLib/Translations/xmilib_pt.ts
@@ -5,38 +5,47 @@
     <name>DebugLogWindow</name>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="51"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="135"/>
         <source>Show:</source>
         <translation>Mostrar:</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="58"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="136"/>
         <source>Infos</source>
         <translation>Informações</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="68"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="137"/>
         <source>Warnings</source>
         <translation>Avisos</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="78"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="138"/>
         <source>Errors</source>
         <translation>Erros</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="101"/>
         <location filename="../DebugLog/DebugLogWindow.ui" line="118"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="130"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="139"/>
         <source>C&amp;lear Log</source>
         <translation>&amp;Limpar Registro</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="108"/>
         <location filename="../DebugLog/DebugLogWindow.ui" line="126"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="134"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="140"/>
         <source>&amp;Close</source>
         <translation>Fe&amp;char</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="121"/>
+        <location filename="../GeneratedFiles/ui_DebugLogWindow.h" line="132"/>
         <source>Ctrl+X</source>
         <translation>Ctrl+X</translation>
     </message>
@@ -44,12 +53,12 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../File/CsvIO.cpp" line="204"/>
+        <location filename="../File/CsvIO.cpp" line="208"/>
         <source>An error occured while writing data to&apos;%1&apos;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../File/CsvIO.cpp" line="212"/>
+        <location filename="../File/CsvIO.cpp" line="216"/>
         <source>Could not open file &apos;%1&apos; for writing.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -82,18 +91,24 @@
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
+        <location filename="../GeneratedFiles/ui_StyleSheetEditor.h" line="99"/>
+        <location filename="../GeneratedFiles/ui_StyleSheetEditor.h" line="101"/>
         <source>&amp;OK</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
+        <location filename="../GeneratedFiles/ui_StyleSheetEditor.h" line="100"/>
+        <location filename="../GeneratedFiles/ui_StyleSheetEditor.h" line="102"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Cancelar</translation>
     </message>
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
+        <location filename="../GeneratedFiles/ui_StyleSheetEditor.h" line="98"/>
+        <location filename="../GeneratedFiles/ui_StyleSheetEditor.h" line="103"/>
         <source>&amp;Apply</source>
         <translation>&amp;Aplicar</translation>
     </message>
@@ -103,6 +118,8 @@
     <message>
         <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
         <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
+        <location filename="../GeneratedFiles/ui_ThreadedOperationDialog.h" line="108"/>
+        <location filename="../GeneratedFiles/ui_ThreadedOperationDialog.h" line="109"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Cancelar</translation>
     </message>
