82237aa11b738a97fd1b56e01aa221dec3a9811a
Xavier Michelon <xavier@michelon.ch>
2022-01-11 06:56:42 +0100

    Added simplified Chinese translation.

--- XMiLib/Translations/xmilib_zh_CN.ts
+++ XMiLib/Translations/xmilib_zh_CN.ts
@@ -6,39 +6,39 @@
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="51"/>
         <source>Show:</source>
-        <translation type="unfinished"></translation>
+        <translation>显示：</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="58"/>
         <source>Infos</source>
-        <translation type="unfinished"></translation>
+        <translation>信息</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="68"/>
         <source>Warnings</source>
-        <translation type="unfinished"></translation>
+        <translation>告警</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="78"/>
         <source>Errors</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="101"/>
         <location filename="../DebugLog/DebugLogWindow.ui" line="118"/>
         <source>C&amp;lear Log</source>
-        <translation type="unfinished"></translation>
+        <translation>清空日志（&amp;l）</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="108"/>
         <location filename="../DebugLog/DebugLogWindow.ui" line="126"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭（&amp;C）</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLogWindow.ui" line="121"/>
         <source>Ctrl+X</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+X</translation>
     </message>
 </context>
 <context>
@@ -46,12 +46,12 @@
     <message>
         <location filename="../File/CsvIO.cpp" line="208"/>
         <source>An error occurred while writing data to&apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>在将数据写入“%1”时，发生了一个错误。</translation>
     </message>
     <message>
         <location filename="../File/CsvIO.cpp" line="216"/>
         <source>Could not open file &apos;%1&apos; for writing.</source>
-        <translation>無法開起%1檔案進行寫入.</translation>
+        <translation>无法打开要写入的“%1”文件。</translation>
     </message>
 </context>
 <context>
@@ -59,22 +59,22 @@
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="58"/>
         <source>&amp;Add</source>
-        <translation type="unfinished"></translation>
+        <translation>添加（&amp;A）</translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="65"/>
         <source>&amp;Remove</source>
-        <translation>移除(&amp;R)</translation>
+        <translation>移除（&amp;R）</translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="106"/>
         <source>&amp;OK</source>
-        <translation>&amp;OK</translation>
+        <translation>确定（&amp;O）</translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.ui" line="113"/>
         <source>&amp;Cancel</source>
-        <translation>取消(&amp;C)</translation>
+        <translation>取消（&amp;C）</translation>
     </message>
 </context>
 <context>
@@ -83,19 +83,19 @@
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
         <source>&amp;OK</source>
-        <translation type="unfinished">&amp;OK</translation>
+        <translation>确定（&amp;O）</translation>
     </message>
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
         <source>&amp;Cancel</source>
-        <translation>取消(&amp;C)</translation>
+        <translation>取消（&amp;C）</translation>
     </message>
     <message>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
         <location filename="../StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
         <source>&amp;Apply</source>
-        <translation>套用(&amp;A)</translation>
+        <translation>应用（&amp;A）</translation>
     </message>
 </context>
 <context>
@@ -104,7 +104,7 @@
         <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
         <location filename="../ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished">取消(&amp;C)</translation>
+        <translation>取消（&amp;C）</translation>
     </message>
 </context>
 <context>
@@ -112,12 +112,12 @@
     <message>
         <location filename="../DebugLog/DebugLog.cpp" line="223"/>
         <source>Date/Time</source>
-        <translation type="unfinished"></translation>
+        <translation>日期/时间</translation>
     </message>
     <message>
         <location filename="../DebugLog/DebugLog.cpp" line="224"/>
         <source>Message</source>
-        <translation>訊息</translation>
+        <translation>信息</translation>
     </message>
 </context>
 <context>
@@ -127,14 +127,14 @@
         <location filename="../String/StringListEditorDialog.cpp" line="130"/>
         <location filename="../String/StringListEditorDialog.cpp" line="160"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <location filename="../String/StringListEditorDialog.cpp" line="106"/>
         <location filename="../String/StringListEditorDialog.cpp" line="130"/>
         <location filename="../String/StringListEditorDialog.cpp" line="160"/>
         <source>Internal error: %1</source>
-        <translation>內部錯誤:%1</translation>
+        <translation>内部错误：%1</translation>
     </message>
 </context>
 <context>
@@ -142,7 +142,7 @@
     <message>
         <location filename="../ThreadedOperation/ThreadedOperationDialog.cpp" line="134"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
 </context>
 </TS>
