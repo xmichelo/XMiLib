d9f9a6e93d02428295bcd725d8769b477ca21788
Xavier Michelon <xavier@michelon.ch>
2022-02-25 07:04:25 +0100

    Added Hungarian translation files.

---
+++ XMiLib/Translations/xmilib_hu_HU.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>DebugLogWindow</name>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="51"/>
+        <source>Show:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="58"/>
+        <source>Infos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="68"/>
+        <source>Warnings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="78"/>
+        <source>Errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="101"/>
+        <location filename="DebugLog/DebugLogWindow.ui" line="118"/>
+        <source>C&amp;lear Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="108"/>
+        <location filename="DebugLog/DebugLogWindow.ui" line="126"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLogWindow.ui" line="121"/>
+        <source>Ctrl+X</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="File/CsvIO.cpp" line="208"/>
+        <source>An error occurred while writing data to&apos;%1&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="File/CsvIO.cpp" line="216"/>
+        <source>Could not open file &apos;%1&apos; for writing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StringListEditorDialog</name>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="58"/>
+        <source>&amp;Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="65"/>
+        <source>&amp;Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="106"/>
+        <source>&amp;OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.ui" line="113"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StyleSheetEditor</name>
+    <message>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="38"/>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="66"/>
+        <source>&amp;OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="45"/>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="71"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="52"/>
+        <location filename="StyleSheetEditor/StyleSheetEditor.ui" line="61"/>
+        <source>&amp;Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThreadedOperationDialog</name>
+    <message>
+        <location filename="ThreadedOperation/ThreadedOperationDialog.ui" line="77"/>
+        <location filename="ThreadedOperation/ThreadedOperationDialog.ui" line="112"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::DebugLog</name>
+    <message>
+        <location filename="DebugLog/DebugLog.cpp" line="222"/>
+        <source>Date/Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="DebugLog/DebugLog.cpp" line="223"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::StringListEditorDialog</name>
+    <message>
+        <location filename="String/StringListEditorDialog.cpp" line="105"/>
+        <location filename="String/StringListEditorDialog.cpp" line="129"/>
+        <location filename="String/StringListEditorDialog.cpp" line="159"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="String/StringListEditorDialog.cpp" line="105"/>
+        <location filename="String/StringListEditorDialog.cpp" line="129"/>
+        <location filename="String/StringListEditorDialog.cpp" line="159"/>
+        <source>Internal error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>xmilib::ThreadedOperationDialog</name>
+    <message>
+        <location filename="ThreadedOperation/ThreadedOperationDialog.cpp" line="134"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
